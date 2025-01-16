251bd94d578962d731822a32773a78f41513a0b2
Secu-Design <mailto.devel@secu-design.com>
2025-01-16 10:50:39 +0100

    Implement html elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {EmployeeListComponent} from "./employee-list/employee-list.component";
 import {QualificationsComponent} from "./qualifications/qualifications.component";
+import {EmployeeDetailsComponent} from "./employee-details/employee-details.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, EmployeeListComponent, QualificationsComponent],
+  imports: [CommonModule, EmployeeListComponent, QualificationsComponent, EmployeeDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
