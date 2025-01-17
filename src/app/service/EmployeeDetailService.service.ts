8dc1069bf03bfc552f4455057e4b842e8ff01a57
Revek310 <kocholi8775@gmail.com>
2025-01-17 12:59:43 +0100

    masterlist - employees can now be clicked to show them in the detail component.

---
+++ src/app/service/EmployeeDetailService.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Employee } from '../Employee';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeDetailService {
+  private selectedEmployeeSubject = new BehaviorSubject<Employee | null>(null);
+  selectedEmployee$ = this.selectedEmployeeSubject.asObservable();
+
+  setSelectedEmployee(employee: Employee) {
+    this.selectedEmployeeSubject.next(employee);
+  }
+
+  getSelectedEmployee() {
+    return this.selectedEmployeeSubject.getValue();
+  }
+}
