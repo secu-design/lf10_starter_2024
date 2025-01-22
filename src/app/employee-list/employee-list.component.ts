0ff05aaa7aa36c9de533b7e993af33523cd41b88
jannikST160 <162319345+jannikST160@users.noreply.github.com>
2025-01-22 08:24:24 +0100

    UI Anpassungen

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,36 +1,50 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Observable} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Employee} from "../Employee";
-import {EmployeeService} from "../service/employee.service";
-import {EmployeeDetailService} from "../service/EmployeeDetailService.service";
+import {EmployeeService} from "../employee-service.service";
+import {FormsModule} from "@angular/forms";
+import {getHtmlTagDefinition} from "@angular/compiler";
+import {toSignal} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
-export class EmployeeListComponent
-{
+export class EmployeeListComponent {
+  bearer: string = '';
   employees$: Observable<Employee[]>;
 
+  toggleCheckbox(_id:any)
+  {
+    let id = _id.toString();
+    let checkbox = <HTMLInputElement> document.getElementById(id);
+    let div = <HTMLInputElement> document.getElementById(id +"-test");
+    if (checkbox.checked) {
+      div.style.backgroundColor = 'lightblue'; // Change to your desired color
+      div.style.border = 'lightblue solid 2px';
+      div.style.borderRadius = '4px';
+    } else {
+      div.style.backgroundColor = ''; // Reset to default
+      div.style.border = 'white solid 2px';
+      div.style.borderRadius = '4px';
+    }
+  }
+
   constructor(
-    private employeeService: EmployeeService,
-    private employeeDetailService: EmployeeDetailService) {
+    private http: HttpClient,
+    private employeeService: EmployeeService) {
     this.employees$ = this.employeeService.getEmployees(); // Use the service to get employees
-  }
-
-  onRowClick(employee: any) {
-    this.employeeDetailService.setSelectedEmployee(employee);
-  }
-
-  trackById(item: any): number {
-    return item.id;
   }
 
   ngOnInit(): void {
     this.employeeService.loadData(); // Load the data when the component initializes
   }
+
+  protected readonly toSignal = toSignal;
+  protected readonly toString = toString;
 }
