f99160921e5581f3d374ae6344e7d31ff70aec56
Secu-Design <mailto.devel@secu-design.com>
2025-01-29 16:29:11 +0100

    Adjust design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {EmployeeListComponent} from "./employee-list/employee-list.component";
 import {QualificationsComponent} from "./qualifications/qualifications.component";
 import {EmployeeDetailsComponent} from "./employee-details/employee-details.component";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {faSignOut} from "@fortawesome/free-solid-svg-icons";
+import {TokenService} from "./service/token.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, EmployeeListComponent, QualificationsComponent, EmployeeDetailsComponent],
+  imports: [CommonModule, EmployeeListComponent, QualificationsComponent, EmployeeDetailsComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent
+{
   title = 'lf10StarterNew';
+  protected readonly faSignOut = faSignOut;
+
+  constructor(private tokenService: TokenService)
+  {
+  }
+
+  logout()
+  {
+    this.tokenService.logout();
+  }
 }
